6ff29d0356266179276d8fe4523a8626aabdf324
aimanggle <aimanggle21@gmail.com>
2022-10-12 11:18:22 +0800

    code change

--- app/Views/Student/show.php
+++ app/Views/Student/show.php
@@ -19,7 +19,7 @@
 
 <div class="container-fluid">
     <div class="row">
-        <div class="col-md-12">
+        <div class="col-md-12 d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
           <!-- Button trigger modal -->
           <h1 class="h2">Student</h1>
           <button type="button" class="btn btn-primary float-end" data-bs-toggle="modal" data-bs-target="#exampleModal">
