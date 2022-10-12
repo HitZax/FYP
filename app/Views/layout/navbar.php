978b39d70d683cfeb7a6589c1cab9e62ae3e5b91
aimanggle <aimanggle21@gmail.com>
2022-10-12 10:44:26 +0800

    code change

--- app/Views/layout/navbar.php
+++ app/Views/layout/navbar.php
@@ -1,34 +1,81 @@
-<nav class="navbar navbar-expand-lg bg-light">
-  <div class="container">
-    <a class="navbar-brand" href="#">Online Logbook System</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarNavDropdown">
-      <ul class="navbar-nav">
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">Home</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Features</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Pricing</a>
-        </li>
-        <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Dropdown link
-          </a>
-          <ul class="dropdown-menu">
-            <li><a class="dropdown-item" href="#">Action</a></li>
-            <li><a class="dropdown-item" href="#">Another action</a></li>
-            <li><a class="dropdown-item" href="#">Something else here</a></li>
-          </ul>
-        </li>
-        <ul class="nav navbar-nav float-end">
+<nav class="navbar navbar-expand-lg navbar-light sticky-top px-5" style="background-color: #ffffff; padding-top:1%; padding-bottom:1%; box-shadow: 0 0px 30px 0 rgba(0, 0, 0, 0.2);">
+    <div class="container-fluid">
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <a class="navbar-brand" href="#"><b>Emcee Junior</b></a>
+        <div class="collapse navbar-collapse " id="navbarTogglerDemo03">
+            
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                <li class="nav-item ">
+                    <a class="nav-link active" aria-current="page" href="/explore">Explore</a>
+                </li>
+
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle active" href="#" id="navbarScrollingDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        Database
+                    </a>
+                    <ul class="dropdown-menu" aria-labelledby="navbarScrollingDropdown">
+                                                  
+                        <li>
+                            <a class="dropdown-item" href=/customer><span><i class="fa-solid fa-user"></i> Customer</span></a>
+                        </li>
+                        <li>
+                            <a class="dropdown-item" href="/segment"><span><i class="fa-solid fa-filter"></i> Segment Customer <span class="badge text-bg-warning">On Going</span></span></a>
+                        </li>
+                        
+                        <li>
+                            <a class="dropdown-item" href="/leadform"><span><i class="fa-solid fa-users-rectangle"></i> Lead Form <span class="badge text-bg-warning">On Going</span></span></a>
+                        </li>
+                    </ul>
+                </li>
+
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle active" href="#" id="navbarScrollingDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false"> Event </a>
+                        <ul class="dropdown-menu" aria-labelledby="navbarScrollingDropdown">                      
+                            <li>
+                                <a class="dropdown-item" href="/event"><span><i class="fa-solid fa-calendar-days"></i> Manage Event</span></a>
+                            </li>
+                            <li>
+                                <a class="dropdown-item" href="/registrant"><span><i class="fa-sharp fa-solid fa-graduation-cap"></i>Manage Registrant</span></a>
+                            </li>
+                        </ul>
+                </li>
+
+                <li class="nav-item">
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle active" href="#" id="navbarScrollingDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false"> Whatsapp </a>
+                            <ul class="dropdown-menu" aria-labelledby="navbarScrollingDropdown">                         
+                                <li>
+                                    <a class="dropdown-item" href="/wacampaign"><span><i class="bi bi-megaphone-fill"></i> Manage Whatsapp Campaign</span></a>
+                                </li>
+                                <li>
+                                    <a class="dropdown-item" href="/walead"><span><i class="bi bi-person-rolodex"></i> Manage Whatsapp Lead</span></a>
+                                </li>
+                            </ul>
+                    </li>
+                </li>
+
+                <li class="nav-item">
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle active" href="#" id="navbarScrollingDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false"> Sales </a>
+                            <ul class="dropdown-menu" aria-labelledby="navbarScrollingDropdown">                       
+                                <li>
+                                    <a class="dropdown-item" href="/sales"><span><i class="fa-solid fa-chart-line"></i> Sales Report <span class="badge text-bg-warning">On Going</span></span></a>
+                                </li>
+                            </ul>
+                    </li>
+                </li>
+
+                <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="/manageuser">User <span class="badge text-bg-warning">On Going</span></a>
+                </li>				
+            </ul>
+
+            <ul class="nav navbar-nav">
                 <li class="dropdown">
                     <a href="#" class="dropdown-toggle nav-link active" data-bs-toggle="dropdown" role="button" aria-expanded="false">
-                        <i class="bi bi-person-circle pr-1"></i>  aimanggle21@gmail.com <span class="caret"></span>
+                        <i class="bi bi-person-circle pr-1"></i>  <?= user()->email; ?> <span class="caret"></span>
                     </a>
 
                     <ul class="dropdown-menu dropdown-menu-right" role="menu">  
@@ -40,7 +87,7 @@
                     </ul>
                 </li>
             </ul>
-      </ul>
+
+        </div>
     </div>
-  </div>
 </nav>
