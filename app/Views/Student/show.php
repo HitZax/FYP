3766aca1ada6b7c97ab6bc292639170d1dd9df84
HitZax <luthfil.hd02@gmail.com>
2022-10-12 11:17:28 +0800

    code changed

--- app/Views/Student/show.php
+++ app/Views/Student/show.php
@@ -2,24 +2,32 @@
 
 <?=$this->section('content')?>
 
+<div class="container-fluid px-3 py-3">
+
+<div class="container-fluid">
+  <div class="row">
+    <div class="col-md-12">
+      <?php if(session()->get('message')):?>
+        <div class="alert alert-success alert-dismissible fade show" role="alert">
+          Data has been  <strong></strong> <?=session()->getFlashdata('message');?> </strong> 
+          <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+        </div>
+      <?php endif;?>
+    </div>
+  </div>
+</div>
+
 <div class="container-fluid">
     <div class="row">
         <div class="col-md-12">
-            <!-- Button trigger modal -->
-<button type="button" class="btn btn-primary float-end" data-bs-toggle="modal" data-bs-target="#exampleModal">
-  New Student
-</button>
-
-<div class="container-fluid">
-<?php if(session()->get('message')):?>
-    <div class="alert alert-success alert-dismissible fade show" role="alert">
-   Data has been  <strong></strong> <?=session()->getFlashdata('message');?> </strong> 
-    <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+          <!-- Button trigger modal -->
+          <h1 class="h2">Student</h1>
+          <button type="button" class="btn btn-primary float-end" data-bs-toggle="modal" data-bs-target="#exampleModal">
+            New Student
+          </button>
+        </div>
     </div>
 </div>
-<?php endif;?>
-
-
 
 <!-- Modal -->
 <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
@@ -94,7 +102,7 @@
 </div>
 
 
-
+</div>
 
 
 <?=$this->endsection()?>
--- app/Views/layout/navbar.php
+++ app/Views/layout/navbar.php
@@ -3,73 +3,19 @@
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
-        <a class="navbar-brand" href="#"><b>Emcee Junior</b></a>
+        <a class="navbar-brand" href="#"><b>Online Logbook System</b></a>
         <div class="collapse navbar-collapse " id="navbarTogglerDemo03">
             
             <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                 <li class="nav-item ">
-                    <a class="nav-link active" aria-current="page" href="/explore">Explore</a>
+                    <a class="nav-link active" aria-current="page" href="/explore">Dashboard</a>
                 </li>
-
-                <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle active" href="#" id="navbarScrollingDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                        Database
-                    </a>
-                    <ul class="dropdown-menu" aria-labelledby="navbarScrollingDropdown">
-                                                  
-                        <li>
-                            <a class="dropdown-item" href=/customer><span><i class="fa-solid fa-user"></i> Customer</span></a>
-                        </li>
-                        <li>
-                            <a class="dropdown-item" href="/segment"><span><i class="fa-solid fa-filter"></i> Segment Customer <span class="badge text-bg-warning">On Going</span></span></a>
-                        </li>
-                        
-                        <li>
-                            <a class="dropdown-item" href="/leadform"><span><i class="fa-solid fa-users-rectangle"></i> Lead Form <span class="badge text-bg-warning">On Going</span></span></a>
-                        </li>
-                    </ul>
+                <li class="nav-item ">
+                    <a class="nav-link active" aria-current="page" href="/task">Task</a>
                 </li>
-
-                <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle active" href="#" id="navbarScrollingDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false"> Event </a>
-                        <ul class="dropdown-menu" aria-labelledby="navbarScrollingDropdown">                      
-                            <li>
-                                <a class="dropdown-item" href="/event"><span><i class="fa-solid fa-calendar-days"></i> Manage Event</span></a>
-                            </li>
-                            <li>
-                                <a class="dropdown-item" href="/registrant"><span><i class="fa-sharp fa-solid fa-graduation-cap"></i>Manage Registrant</span></a>
-                            </li>
-                        </ul>
+                <li class="nav-item ">
+                    <a class="nav-link active" aria-current="page" href="/com">Communicate</a>
                 </li>
-
-                <li class="nav-item">
-                    <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle active" href="#" id="navbarScrollingDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false"> Whatsapp </a>
-                            <ul class="dropdown-menu" aria-labelledby="navbarScrollingDropdown">                         
-                                <li>
-                                    <a class="dropdown-item" href="/wacampaign"><span><i class="bi bi-megaphone-fill"></i> Manage Whatsapp Campaign</span></a>
-                                </li>
-                                <li>
-                                    <a class="dropdown-item" href="/walead"><span><i class="bi bi-person-rolodex"></i> Manage Whatsapp Lead</span></a>
-                                </li>
-                            </ul>
-                    </li>
-                </li>
-
-                <li class="nav-item">
-                    <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle active" href="#" id="navbarScrollingDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false"> Sales </a>
-                            <ul class="dropdown-menu" aria-labelledby="navbarScrollingDropdown">                       
-                                <li>
-                                    <a class="dropdown-item" href="/sales"><span><i class="fa-solid fa-chart-line"></i> Sales Report <span class="badge text-bg-warning">On Going</span></span></a>
-                                </li>
-                            </ul>
-                    </li>
-                </li>
-
-                <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="/manageuser">User <span class="badge text-bg-warning">On Going</span></a>
-                </li>				
             </ul>
 
             <ul class="nav navbar-nav">
