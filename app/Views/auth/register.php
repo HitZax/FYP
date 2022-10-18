c4cd5a08ff35defb35fe5a73cff9911328906b07
HitZax <luthfil.hd02@gmail.com>
2022-10-18 23:42:11 +0800

    design

--- app/Views/auth/register.php
+++ app/Views/auth/register.php
@@ -8,6 +8,8 @@
     <title> Register Student | LS </title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+    <link rel="stylesheet" href="asset/css/style.css">
+  <link rel="stylesheet" href="asset/css/library.css">
   </head>
 
   <style>
@@ -34,7 +36,7 @@
         <form action="/register" method="post">
           <div class="mb-3">
             <label class="form-label float-start">Full Name</label>
-              <input type="text" name="name" placeholder="name"  class="form-control" >
+              <input type="text" name="name" placeholder="Full name"  class="form-control" >
               <div class="invalid-feedback">Please enter your Full Name.</div>
           </div>
           <div class="mb-3">
