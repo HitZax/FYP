cc87f99f33381e6a9ed7775d51c4e8c09f7d1f0d
aimanggle <aimanggle21@gmail.com>
2022-10-25 15:51:58 +0800

    cc

--- app/Views/layout/template.php
+++ app/Views/layout/template.php
@@ -7,6 +7,9 @@
     <title> <?=$title?> </title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+
+        <!-- Font Awesome Kit  -->
+        <script src="https://kit.fontawesome.com/e5eb685012.js" crossorigin="anonymous"></script>
   </head>
   <body>
     <?=$this->include('layout/navbar');?>
