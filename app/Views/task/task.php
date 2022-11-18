2d444c806faa2a50d8b416a6497e3f8cee47516d
aimanggle <aimanggle21@gmail.com>
2022-11-19 07:08:52 +0800

    code fix

--- app/Views/task/task.php
+++ app/Views/task/task.php
@@ -2,7 +2,7 @@
 
 <?=$this->section('content')?>
 
-<div class="container mx-aut$routes->get('/task/detail/(:any)', 'Task::edit/$1', ['filter'=> 'authGuard', 'as' => 'task.edit'],);o mt-5">
+<div class="container mx-auto mt-5">
     <div class="row">
       <!-- <div class="col-md-6 offset-md-3 mt-5 mb-5"> -->
       <!-- <div class="shadow-lg"> -->
