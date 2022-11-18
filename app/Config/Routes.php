110bef12925119e73dee39cb3cf0089d5584f813
aimanggle <aimanggle21@gmail.com>
2022-11-19 07:07:56 +0800

    fix : routes in task routes

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -83,8 +83,8 @@ $routes->get('/task/new/(:any)', 'Task::index', ['filter'=> 'authGuard', 'as' =>
 $routes->post('/task/new/(:any)', 'Task::store/$1', ['filter'=> 'authGuard', 'as' => 'task.store'],);
 $routes->get('/task/detail/(:any)', 'Task::edit/$1', ['filter'=> 'authGuard', 'as' => 'task.edit'],);
 $routes->post('/task/detail/(:any)', 'Task::update/$1', ['filter'=> 'authGuard', 'as' => 'task.update'],);
+$routes->get('/task/detail/(:any)', 'Task::show/$1', ['filter'=> 'authGuard', 'as' => 'task.show'],);
 $routes->delete('/task/delete/(:any)', 'Task::delete/$1', ['filter'=> 'authGuard', 'as' => 'task.delete'],);
-$routes->get('/task/new/(:any)', 'Task::show/$1', ['filter'=> 'authGuard', 'as' => 'task.show'],);
 
 $routes->get('/chat', 'Chat::index');
 
