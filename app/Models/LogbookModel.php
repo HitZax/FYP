4c46ed2e4d1735da1c8d8f71a5617b86820b8a6f
aimanggle <aimanggle21@gmail.com>
2022-11-23 07:33:50 +0800

    update logbook model

--- app/Models/LogbookModel.php
+++ app/Models/LogbookModel.php
@@ -7,5 +7,13 @@ use CodeIgniter\Model;
 class LogbookModel extends Model
 {
     protected $table            = 'logbook';
-    protected $allowedFields    = ['lbid','lbcreated', 'sid'];
+    protected $primaryKey       = 'lbid';
+    protected $allowedFields    = ['lbcreated', 'sid', 'lid'];
+
+    public function detail($lid)
+    {
+        $sql = ("SELECT * FROM logbook WHERE lid = $lid");
+        $result = $this->db->query($sql);
+        return $result->getRowArray();
+    }
 }
