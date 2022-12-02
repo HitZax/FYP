1f04d382f632ae0a8acad5debefe1f9a46bee0ef
aimanggle <aimanggle21@gmail.com>
2022-12-02 22:24:44 +0800

    add session helper

---
+++ app/Helpers/ses_helper.php
@@ -0,0 +1,31 @@
+<?php
+
+//function get session user id
+function userid()
+{
+    return session()->get('id');
+}
+
+//function get session username
+function fullname()
+{
+    return session()->get('fullname');
+}
+
+//function get session user role
+function studentid()
+{
+    return session()->get('studentid');
+}
+
+//function get session user email
+function useremail()
+{
+    return session()->get('email');
+}
+
+//function get session user role
+function userrole()
+{
+    return session()->get('role');
+}
